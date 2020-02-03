0179b4dbb73a67a5f3c8a8dcd68b2eab25942ff4
syazshamsudin <52715379+syazshamsudin@users.noreply.github.com>
2020-02-03 15:43:53 +0800

    Updated 3/2/2020

---
+++ EventScheduler/app/src/main/java/com/example/eventscheduler/StudentProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.eventscheduler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentProfileActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_profile_page);
+
+    }
+}
